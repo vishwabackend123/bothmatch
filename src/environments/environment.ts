1da4843d6e2e0f924d750fef52ce3d8baca55b59
Vishwa <vishvamitra.yadav@vlinkinfo.com>
2023-02-13 17:15:44 +0530

    replace site key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   usaJobsUrl: "https://data.usajobs.gov/api",
   baseUrl:"https://2nfrmwo081.execute-api.us-east-1.amazonaws.com/dev/",
   // siteKey: "6LcD6aAcAAAAAFucR0IcVnwv4AuVDve7wEbN0dnd",
-  siteKey: "6LdMJdcZAAAAACxFRcp-pOKFICutsSI_DZkZUo6y",
+  //siteKey: "6LdMJdcZAAAAACxFRcp-pOKFICutsSI_DZkZUo6y",
+  siteKey: "6LcO7ngkAAAAAEiytzWkoIwYywfP819FFcA4LzSQ", //http://dev-bothmatch-vlink.s3-website-us-east-1.amazonaws.com
   passwordEncryptionKey: "ZKzeE1JApIA_ycm-nuaI5R8PrZDNgofKH05FOOgCsmI=",
   backendBaseUrl :"https://2nfrmwo081.execute-api.us-east-1.amazonaws.com/dev/",
   imgBaseUrl:"http://ec2-54-211-175-15.compute-1.amazonaws.com:8080",
